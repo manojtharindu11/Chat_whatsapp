b4040e74aafc88768e93b7fbc25577729a071c3f
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2025-06-27 12:47:59 +0530

    refactor: restructure chat components and update imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import Chat from "./chat";
+import Chat from "./components/chat";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <div className="app">
-        <h2 className="app-header">WhatsApp Chat Sim</h2>
+        <h2 className="app-header">Chat</h2>
         <Chat />
       </div>
     </>
