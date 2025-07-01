31dc9898061516d7a5b2f4eb695c05de0f02f302
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2025-07-01 23:48:03 +0530

    feat: implement message sending functionality and update server port configuration

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,21 @@
 from gevent import monkey
+
 monkey.patch_all()
 
 from flask import Flask, request
 from flask_socketio import SocketIO
 from gevent.lock import Semaphore  # gevent-safe lock
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()  # load variables from .env
+
+PORT = os.getenv("PORT", 5010)
+WEBHOOK_VERIFY_TOKEN = os.getenv("WEBHOOK_VERIFY_TOKEN")
+GRAPH_API_TOKEN = os.getenv("GRAPH_API_TOKEN")
+YOUR_PHONE_NUMBER_ID = os.getenv("YOUR_PHONE_NUMBER_ID")
 
 app = Flask(__name__)
 CORS(app)
@@ -18,8 +29,36 @@ clients_lock = Semaphore()
 clients = [
     {"id": 1, "name": "Nilanthini", "whatsapp": "0771234567"},
     {"id": 2, "name": "Chamod", "whatsapp": "0777654321"},
-    {"id": 3, "name": "Manoj", "whatsapp": "0763241208"},
+    {"id": 3, "name": "Manoj", "whatsapp": "94763241208"},
 ]
+
+def send_message(data):
+    body = data["content"]
+    client_id = data["to"]
+    business_phone_number_id = YOUR_PHONE_NUMBER_ID
+    recipient_phone_number = [client["whatsapp"] for client in clients if client["id"] == client_id]
+    url = f'https://graph.facebook.com/v18.0/{business_phone_number_id}/messages'
+
+    headers = {
+        "Authorization": f"Bearer {GRAPH_API_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": recipient_phone_number,
+        "type": "text",
+        "text": {
+            "body": body
+        }
+    }
+
+    response = requests.post(url=url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        print("✅ Message sent successfully")
+    else:
+        print("❌ Failed to send message:", response.status_code, response.text)
 
 @app.route("/clients", methods=["GET"])
 def get_clients():
@@ -52,6 +91,7 @@ def handle_send_message(data):
     if to_sid:
         # Send message to a specific client
         print(f"Sending message to {to_sid}: {data}")
+        send_message(data)
         socketio.emit("receive_message", data, to=to_sid)
     else:
         # Broadcast message to all clients
@@ -70,7 +110,7 @@ if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
 
-    print("Starting Socket.IO server on ws://localhost:5002")
+    print(f"Starting Socket.IO server on ws://localhost:{PORT}")
     # Start the WSGI server with WebSocket handler
-    server = pywsgi.WSGIServer(("0.0.0.0", 5002), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(("0.0.0.0", int(PORT)), app, handler_class=WebSocketHandler)
     server.serve_forever()
